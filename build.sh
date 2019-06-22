7cfa469dbf811c90ab5ccd324019830409048273
John Goodliff <johny112009@hotmail.com>
2019-06-22 16:59:45 -0600

    Fixed build script. Fixed broken styles from last Outlook update. Rewrote styles to help prevent breaking in the future from changing classes. Incremented build number

--- build.sh
+++ build.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # Copy files to build/, builds css file, and zips
-rm -rf build && mkdir -p build && cp README.md LICENSE.md manifest.json 128.png 48.png 32.png 16.png build/ && sass style.scss build/style.css --style=compressed --no-source-map && zip -rj build/dark-mode-for-outlook.zip build/* && echo "BUILD SUCCESSFUL"
+rm -rf build && mkdir -p build && cp README.md LICENSE.md manifest.json 128.png 48.png 32.png 16.png build/ && sass style.scss build/style.css --style=compressed --sourcemap=none && zip -rj build/dark-mode-for-outlook.zip build/* && echo "BUILD SUCCESSFUL"
