462f663ac97ca59cff4a6acf6edb583f2d19643e
John Goodliff <johny112009@hotmail.com>
2020-04-24 17:58:10 -0600

    Update styles to work with the latest Outlook update. Minor change to build script

--- build.sh
+++ build.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # Copy files to build/, builds css file, and zips
-rm -rf build && mkdir -p build && cp -r README.md LICENSE.md manifest.json 128.png 48.png 32.png 16.png _locales build/ && sass style.scss build/style.css --style=compressed --sourcemap=none && cd build/ && zip -r dark-mode-for-outlook.zip * && time="`date +%T`"; echo "✓ BUILD SUCCESSFUL ("$time")"
+rm -rf build && mkdir -p build && cp -r README.md LICENSE.md manifest.json 128.png 48.png 32.png 16.png _locales build/ && sass style.scss build/style.css --style=compressed --no-source-map && cd build/ && zip -r dark-mode-for-outlook.zip * && time="`date +%T`"; echo "✓ BUILD SUCCESSFUL ("$time")"
