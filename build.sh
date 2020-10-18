05610b1d3b849084d0608681ecf035d0c494e373
John Goodliff <johny112009@hotmail.com>
2020-10-18 17:48:14 -0600

    Split styles into separate files and change loading to document_start. Add more style overrides for black text. Add styles for Skype and Help panes. Other changes

--- build.sh
+++ build.sh
@@ -1,3 +1,10 @@
 #!/bin/bash
 # Copy files to build/, builds css file, and zips
-rm -rf build && mkdir -p build && cp -r README.md LICENSE.md manifest.json 128.png 64.png 48.png 32.png 16.png _locales build/ && sass style.scss build/style.css --style=compressed --no-source-map && cd build/ && zip -r dark-mode-for-outlook.zip * && time="`date +%T`"; echo "✓ BUILD COMPLETE ("$time")"
+rm -rf build && \
+mkdir -p build && \
+cp -r README.md LICENSE.md manifest.json 128.png 64.png 48.png 32.png 16.png _locales build/ && \
+sass styles/main.scss build/main.css --style=compressed --no-source-map && \
+sass styles/help.scss build/help.css --style=compressed --no-source-map && \
+cd build/ && \
+zip -r dark-mode-for-outlook.zip * && \
+time="`date +%T`"; echo "✓ BUILD COMPLETE ("$time")"
