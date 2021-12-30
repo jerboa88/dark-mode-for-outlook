14d92fee20b52205d1a946ff86e4705de1730c60
John Goodliff <johny112009@hotmail.com>
2021-12-29 22:02:08 -0700

    Refactor build script. Move store listing text from messages.json to separate files. Add source code and Opera store listing text generation to build script

--- build.js
+++ build.js
@@ -2,222 +2,310 @@
 	'use strict';
 
 	// Deps
+	const archiver = require('archiver');
 	const fs = require('fs-extra');
-	const { join } = require('path');
+	const { join, basename, resolve } = require('path');
+	const { performance } = require('perf_hooks');
+	const NoBS = require('NoBS');
 	const replace = require('replace-in-file');
 	const { renderSync } = require('sass');
 	const sharp = require('sharp');
-	const { sync: { zip } } = require('zip-local');
+
 
 	// Constants
 	const buildFolder = 'build';
 	const firefoxSubfolder = join(buildFolder, 'firefox');
 	const chromiumSubfolder = join(buildFolder, 'chromium');
+	const deliverablesFolder = join(buildFolder, 'deliverables');
 	const localeFolder = '_locales';
 	const imgFolder = 'img';
 	const iconFilename = 'icon.svg';
 	const packageJson = 'package.json';
-	const messagesJson = 'messages.json';
 	const manifestJson = 'manifest.json';
+	const storeListingTxt = 'store-listing.txt';
 	const scssFiles = ['css/main.scss', 'css/help.scss', 'css/compose.scss'];
-	const filesToCopy = ['README.md', 'LICENSE.md', manifestJson, 'options.html', 'js/options.js', 'js/background.js', 'js/content.js', '_locales'];
+	const filesToCopy = ['README.md', 'LICENSE.md', manifestJson, 'options.html', 'js/options.js', 'js/background.js', 'js/content.js', localeFolder];
 
-	// Runtime
+
+	// Runtime vars
 	let extensionName;
 	let extensionVersion;
 
 
-	// Shows a success message
-	const continueBuild = async (msg) => {
-		console.log('✓', msg);
-	};
-
-	// Stops the build and shows an error messages
-	const cancelBuild = async (msg, error) => {
-		console.error('✗', msg);
-		console.error(error);
-
-		process.exit(1);
-	};
-
 	// Get extension name and version from package.json
 	const getExtensionDetails = async () => {
-		try {
-			const packageObj = await fs.readJson(packageJson);
+		return new Promise(async (resolve, reject) => {
+			try {
+				const packageObj = await fs.readJson(packageJson);
 
-			extensionName = packageObj.name;
-			extensionVersion = packageObj.version.replace('.0', '');
-		} catch (error) {
-			cancelBuild('Error while getting the extension details from ' + packageJson, error);
-		}
+				// TODO: Use destructuring here
+				extensionName = packageObj.name;
+				extensionVersion = packageObj.version.replace('.0', '');
 
-		continueBuild('Starting build of ' + extensionName + ' v' + extensionVersion);
-	};
-
-	// Generate zip filenames based on the extension name, version, and variant
-	const getZipFileName = (extensionVariant) => {
-		return extensionName + '-' + extensionVersion + '-' + extensionVariant + '.zip';
-	};
-
-	// Generate zip files
-	const zipBuildFolders = async () => {
-		try {
-			zip(firefoxSubfolder).compress().save(join(buildFolder, getZipFileName('firefox')));
-			zip(chromiumSubfolder).compress().save(join(buildFolder, getZipFileName('chromium')));
-
-			continueBuild('Files zipped');
-		} catch (error) {
-			cancelBuild('Error while zipping files', error);
-		}
-	};
-
-	// Customize manifest for different extension variants so that warnings aren't shown on install
-	// (Chrome: Remove "browser_specific_settings" section. Firefox: Make background page persistent)
-	const customizeManifest = async () => {
-		try {
-			const chromiumResult = await replace({
-				files: join(chromiumSubfolder, manifestJson),
-				from: /\t{0,4}"browser_specific_settings": ?[\s\S]{0,128}\},\s/,
-				to: '',
-				countMatches: true
-			});
-
-			const firefoxResult = await replace({
-				files: join(firefoxSubfolder, manifestJson),
-				from: /"persistent": ?false/,
-				to: '"persistent": true',
-				countMatches: true
-			});
-
-			if (!chromiumResult || chromiumResult.length != 1 || !chromiumResult[0].hasChanged) {
-				throw new Error(`Section "browser_specific_settings" could not be found in ${manifestJson}`);
-			} else if (!firefoxResult || firefoxResult.length != 1 || !firefoxResult[0].hasChanged) {
-				throw new Error(`Option "persistent" could not be found in ${manifestJson}`);
+				resolve();
+			} catch (error) {
+				reject(error);
 			}
-
-			continueBuild('Manifest modified');
-		} catch (error) {
-			cancelBuild('Error while modifying manifest', error);
-		}
-	};
-
-	// Duplicate Firefox build folder to make other build variants
-	const duplicateBuildFolder = async () => {
-		try {
-			await fs.copy(firefoxSubfolder, chromiumSubfolder);
-
-			continueBuild('Duplicated build');
-		} catch (error) {
-			cancelBuild('Error while duplicating build', error);
-		}
-	};
-
-	// Generate extension icons in multiple sizes
-	const generateIcons = async () => {
-		try {
-			const outputImgFolder = join(firefoxSubfolder, imgFolder);
-
-			await fs.ensureDir(outputImgFolder)
-
-			await Promise.all([128, 64, 48, 32, 16].map(size => {
-				return sharp(join(imgFolder, iconFilename))
-					.resize(size, size)
-					.png({
-						compressionLevel: 9,
-						adaptiveFiltering: true,
-						palette: true
-					})
-					.toFile(join(outputImgFolder, `${size}.png`))
-			}));
-
-			continueBuild('Icons generated');
-		} catch (error) {
-			cancelBuild('Error while generating icons', error);
-		}
-	};
-
-	// Compile SCSS to CSS
-	const compileScss = async () => {
-		let scssCompilationError = false;
-
-		scssFiles.forEach(filename => {
-			const css = renderSync({
-				file: filename,
-				outputStyle: 'compressed',
-				sourceMap: false
-			}).css;
-
-			fs.outputFile(join(firefoxSubfolder, filename.replace('.scss', '.css')), css, error => {
-				if (error) {
-					scssCompilationError = error;
-				}
-			});
 		});
-
-		scssCompilationError ? cancelBuild('Error while compiling scss', scssCompilationError) : continueBuild('SCSS compiled');
 	};
 
-	// Copy files to build folder
-	const copyFiles = async () => {
-		const copyPromises = filesToCopy.map(filename => {
-			return fs.copy(filename, join(firefoxSubfolder, filename));
-		});
-
-		try {
-			await Promise.all(copyPromises);
-
-			continueBuild('Files copied');
-		} catch (error) {
-			cancelBuild('Error while copying files', error);
-		}
-	};
 
 	// Create a build folder if it doesn't exist already, otherwise empty it
 	const cleanBuildFolder = async () => {
-		try {
-			await fs.emptyDir(buildFolder);
-			await fs.emptyDir(firefoxSubfolder);
-			await fs.emptyDir(chromiumSubfolder);
+		return new Promise(async (resolve, reject) => {
+			try {
+				await fs.emptyDir(buildFolder);
+				await Promise.all([fs.emptyDir(firefoxSubfolder), fs.emptyDir(chromiumSubfolder), fs.emptyDir(deliverablesFolder)]);
 
-			continueBuild('Cleaned build folder');
-		} catch (error) {
-			cancelBuild('Error while cleaning build folder', error);
-		}
+				resolve();
+			} catch (error) {
+				reject(error);
+			}
+		});
 	};
 
-	// Generate complete store listing descriptions from localized messages
-	const generateStoreListings = async () => {
-		const localeCodes = (() => fs.readdirSync(localeFolder).filter(filename => fs.statSync(join(localeFolder, filename)).isDirectory()))();
 
-		if (localeCodes !== null) {
-			localeCodes.forEach(localeCode => {
-				const messages = JSON.parse(fs.readFileSync(join(localeFolder, localeCode, messagesJson)));
-				const listingText = Object.keys(messages)
-					.filter(key => key.substring(0, 5) === 'store')
-					.map(key => messages[key].message)
-					.join('\n\n');
-
-				fs.writeFile(join(buildFolder, 'store-listing-' + localeCode + '.txt'), listingText, error => {
-					if (error) {
-						cancelBuild('Error while generating store listings for' + localeCode.toUpperCase(), error);
-					} else {
-						continueBuild('Store listing generated for ' + localeCode.toUpperCase());
-					}
+	// Customize manifest for different extension variants so that warnings aren't shown on install:
+	// - For Chromium, remove "browser_specific_settings" section
+	// - For Firefox, make background page persistent)
+	const customizeManifests = async () => {
+		return new Promise(async (resolve, reject) => {
+			try {
+				const chromiumResult = await replace({
+					files: join(chromiumSubfolder, manifestJson),
+					from: /\t{0,4}"browser_specific_settings": ?[\s\S]{0,128}\},\s/,
+					to: '',
+					countMatches: true
 				});
-			});
-		}
+
+				const firefoxResult = await replace({
+					files: join(firefoxSubfolder, manifestJson),
+					from: /"persistent": ?false/,
+					to: '"persistent": true',
+					countMatches: true
+				});
+
+				if (!chromiumResult || chromiumResult.length != 1 || !chromiumResult[0].hasChanged) {
+					throw new Error(`Section "browser_specific_settings" could not be found in ${manifestJson}`);
+				} else if (!firefoxResult || firefoxResult.length != 1 || !firefoxResult[0].hasChanged) {
+					throw new Error(`Option "persistent" could not be found in ${manifestJson}`);
+				}
+
+				resolve();
+			} catch (error) {
+				reject(error);
+			}
+		});
 	};
 
-	await getExtensionDetails();
-	await cleanBuildFolder();
-	await copyFiles();
-	await compileScss();
-	await generateIcons();
-	await duplicateBuildFolder();
-	await customizeManifest();
-	await zipBuildFolders();
-	await generateStoreListings();
 
-	console.log();
-	continueBuild('BUILD COMPLETE (' + new Date().toLocaleTimeString() + ')\n');
+	// Duplicate Firefox build folder to make other build variants
+	const duplicateBuildFolder = async () => {
+		return new Promise(async (resolve, reject) => {
+			try {
+				await fs.copy(firefoxSubfolder, chromiumSubfolder);
+
+				resolve();
+			} catch (error) {
+				reject(error);
+			}
+		});
+	};
+
+
+	// Generate extension icons in multiple sizes
+	const generateIcons = async () => {
+		return new Promise(async (resolve, reject) => {
+			try {
+				const outputImgFolder = join(firefoxSubfolder, imgFolder);
+
+				await fs.ensureDir(outputImgFolder);
+				await Promise.all([128, 64, 48, 32, 16].map(size => {
+					return sharp(join(imgFolder, iconFilename))
+						.resize(size, size)
+						.png({
+							compressionLevel: 9,
+							adaptiveFiltering: true,
+							palette: true
+						})
+						.toFile(join(outputImgFolder, `${size}.png`));
+				}));
+
+				resolve();
+			} catch (error) {
+				reject(error);
+			}
+		});
+	};
+
+
+	// Compile SCSS to CSS
+	const compileScss = async () => {
+		return new Promise(async (resolve, reject) => {
+			try {
+				scssFiles.forEach(filename => {
+					const css = renderSync({
+						file: filename,
+						outputStyle: 'compressed',
+						sourceMap: false
+					}).css;
+
+					fs.outputFile(join(firefoxSubfolder, filename.replace('.scss', '.css')), css, error => {
+						if (error) {
+							throw new Error(error);
+						}
+					});
+				});
+
+				resolve();
+			} catch (error) {
+				reject(error);
+			}
+		});
+	};
+
+
+	// Copy source files to build folder
+	const copyFiles = async () => {
+		return new Promise(async (resolve, reject) => {
+			try {
+				const copyTasks = filesToCopy.map(filename => {
+					return fs.copy(filename, join(firefoxSubfolder, filename), {
+						filter: file => {
+							// Filter out store-listing.txt files because we will generate these later
+							return !file.includes(storeListingTxt);
+						}
+					});
+				});
+
+				await Promise.all(copyTasks);
+
+				resolve();
+			} catch (error) {
+				reject(error);
+			}
+		});
+	};
+
+
+	// Generate complete store listing descriptions from localized store-listing.txt files
+	const generateStoreListings = async () => {
+		return new Promise(async (resolve, reject) => {
+			const localeCodes = (() => fs.readdirSync(localeFolder)
+				.filter(filename => fs.statSync(join(localeFolder, filename)).isDirectory())
+			)();
+
+			if (localeCodes !== null) {
+				localeCodes.forEach(localeCode => {
+					const text = fs.readFileSync(join(localeFolder, localeCode, storeListingTxt)).toString();
+					const textForOpera = text.replace(/(?:🔶|🔸)/g, '♦').replace(/🙂/g, ':)');
+
+					fs.writeFile(join(deliverablesFolder, `store-listing-${localeCode}.txt`), text, error => {
+						if (error) {
+							reject(error);
+						}
+
+						resolve();
+					});
+
+					fs.writeFile(join(deliverablesFolder, `store-listing-${localeCode}-opera.txt`), textForOpera, error => {
+						if (error) {
+							reject(error);
+						}
+
+						resolve();
+					});
+				});
+			}
+		});
+	};
+
+
+	// Helper function to zip files and folders
+	const zip = async (inputFolder, globPattern, ignoreGlobPatterns, outputFolder, filenameLabel) => {
+		return new Promise(async (resolve, reject) => {
+			const filename = `${extensionName}-${extensionVersion}-${filenameLabel}.zip`;
+			const output = fs.createWriteStream(join(outputFolder, filename));
+			const archive = archiver('zip', {
+				zlib: { level: 9 }
+			});
+
+			// Fired once all archive data has been written
+			output.on('close', () => resolve());
+			archive.on('error', error => reject(error));
+
+			// Zip all files and place in root directory of zip, excluding certain files/folders
+			archive.pipe(output);
+			archive.glob(globPattern, { cwd: inputFolder, ignore: ignoreGlobPatterns }, { prefix: '' });
+
+			// Finalize the archive. We still need to wait for streams to finish
+			archive.finalize();
+		});
+	};
+
+
+	// Create zip file of the Chromium build folder
+	const zipChromiumBuildFolder = async () => {
+		return new Promise(async (resolve, reject) => {
+			try {
+				await zip(chromiumSubfolder, '**', [], buildFolder, 'chromium');
+
+				resolve();
+			} catch (error) {
+				reject(error);
+			}
+		});
+	};
+
+
+	// Create zip file of the Firefox build folder
+	const zipFirefoxBuildFolder = async () => {
+		return new Promise(async (resolve, reject) => {
+			try {
+				await zip(firefoxSubfolder, '**', [], buildFolder, 'firefox');
+
+				resolve();
+			} catch (error) {
+				reject(error);
+			}
+		});
+	};
+
+
+	// Create zip file of the project source code (required by Firefox web store)
+	const zipSourceCode = async () => {
+		return new Promise(async (resolve, reject) => {
+			try {
+				await zip('', '**', ['node_modules/**', 'build/**'], deliverablesFolder, 'source-firefox');
+
+				resolve();
+			} catch (error) {
+				reject(error);
+			}
+		});
+	};
+
+
+	const main = () => {
+		// Items in this array are completed sequentially. Tasks on the same level are run concurrently to save time
+		const taskList = [
+			[getExtensionDetails, cleanBuildFolder],
+			[copyFiles, compileScss, generateIcons, zipSourceCode, generateStoreListings],
+			[duplicateBuildFolder],
+			[customizeManifests],
+			[zipFirefoxBuildFolder, zipChromiumBuildFolder]
+		];
+
+		const noBS = new NoBS(taskList);
+		const startTime = performance.now();
+
+		console.log('Starting build\n');
+
+		noBS.run()
+			.then(result => console.log(`\nFinished in ${((performance.now() - startTime) / 1000).toFixed(2)} seconds\n`))
+			.catch(error => console.error(`\nError: ${error}\n`));
+	}
+
+
+	main();
 })();
